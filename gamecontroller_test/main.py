7513b8baacfe8db01e0f2358803f6901f20a7071
gaojunxuan <gaojunxuanbox@live.com>
2019-03-06 09:40:46 -0600

    Update the code so that it can read instructions simultaneously

--- gamecontroller_test/main.py
+++ gamecontroller_test/main.py
@@ -1,7 +1,9 @@
 from evdev import InputDevice, categorize, ecodes
 from enum import Enum
+from nanpy import SerialManager, ArduinoApi
 import evdev
 import serial
+import nanpy
 
 class Direction(Enum):
     FORWARD = 0
@@ -14,15 +16,28 @@ class Direction(Enum):
     DOWN = 7
     NEUTRAL = 8
     UNKNOWN = 9
+    NEUTRALL = 10
+    NEUTRALR = 11
 
-ser = serial.Serial("/dev/ttyACM0", 9600)
+arduino_conn = None
+try:
+    arduino_conn = SerialManager()
+    arduino_conn.open()
+except:
+    print("Couldn't find the Arduino. Check the connection.")
+
+arduino_api = None
+if arduino_conn is not None:
+    arduino_api = ArduinoApi(connection=arduino_conn)
 
 class Instruction:
-    def __init__(self, direction):
+    def __init__(self, direction, source=""):
         self.direction = direction
+        self.source = source;
 
     def send_instruction(self):
-        ser.write(b'%d'  % self.direction.value)
+        pass
+        #ser.write(b'%d'  % self.direction.value)
 
 def resolve_absevent(abs_event):
     if abs_event.type == ecodes.EV_ABS:
@@ -32,25 +47,33 @@ def resolve_absevent(abs_event):
         #print(value)
         if type_code == "ABS_X":
             if value < 128:
-                return Instruction(Direction.LEFT)
+                return Instruction(Direction.LEFT, "left")
             elif value > 128:
-                return Instruction(Direction.RIGHT)
+                return Instruction(Direction.RIGHT, "left")
+            else:
+                return Instruction(Direction.NEUTRALL, "left")
         elif type_code == "ABS_Y":
             if value < -129:
-                return Instruction(Direction.FORWARD)
+                return Instruction(Direction.FORWARD, "left")
             elif value > -129:
-                return Instruction(Direction.BACKWARD)
+                return Instruction(Direction.BACKWARD, "left")
+            else:
+                return Instruction(Direction.NEUTRALL, "left")
         elif type_code == "ABS_RX":
             if value < 128:
-                return Instruction(Direction.COUNTERCLOCKWISE)
+                return Instruction(Direction.COUNTERCLOCKWISE, "right")
             elif value > 128:
-                return Instruction(Direction.CLOCKWISE)
+                return Instruction(Direction.CLOCKWISE, "right")
+            else:
+                return Instruction(Direction.NEUTRALR, "right")
         elif type_code == "ABS_RY":
             if value < -129:
-                return Instruction(Direction.UP)
+                return Instruction(Direction.UP, "right")
             elif value > -129:
-                return Instruction(Direction.DOWN)
-        return Instruction(Direction.NEUTRAL)
+                return Instruction(Direction.DOWN, "right")
+            else:
+                return Instruction(Direction.NEUTRALR, "right")
+        #return Instruction(Direction.NEUTRAL)
     return Instruction(Direction.UNKNOWN)
 
 def resolve_btnevent(btn_event):
@@ -83,9 +106,9 @@ if device_path == "":
     print("Couldn't find the gamepad. Check the connection.")
 else:
     print("Listening on: ", device_path)
-    print("Sending to: ", "/dev/ttyACM0")
+    print("Sending to: ", arduino_conn.device)
     gamepad = InputDevice(device_path)
-    previous_instruction = Instruction(Direction.NEUTRAL)
+    previous_instruction = [Instruction(Direction.NEUTRALL, "left"), Instruction(Direction.NEUTRALR, "right")]
     for event in gamepad.read_loop():
         if event.type == ecodes.EV_KEY:
             instruction = resolve_btnevent(event)
@@ -93,13 +116,27 @@ else:
             print(instruction.direction)
         elif event.type == ecodes.EV_ABS:
             current_instruction = resolve_absevent(event)
-            if current_instruction.direction == Direction.NEUTRAL:
-                current_instruction.send_instruction()
-                print(current_instruction.direction)
-            if previous_instruction is not None and previous_instruction.direction != current_instruction.direction:
+            
+            if current_instruction.source == "left":
+                if current_instruction.direction == Direction.NEUTRALL:
+                    print(current_instruction.direction)
+                if previous_instruction is not None and previous_instruction[0].direction != current_instruction.direction:
+                    if previous_instruction[0].direction == Direction.NEUTRALL:
+                        print(current_instruction.direction)
+                previous_instruction[0] = current_instruction
+            
+            if current_instruction.source == "right":
+                if current_instruction.direction == Direction.NEUTRALR:
+                    print(current_instruction.direction)
+                if previous_instruction is not None and previous_instruction[1].direction != current_instruction.direction:
+                    if previous_instruction[1].direction == Direction.NEUTRALR:
+                        print(current_instruction.direction)
+                previous_instruction[1] = current_instruction
+                    
+            '''if previous_instruction is not None and previous_instruction.direction != current_instruction.direction:
                 if previous_instruction.direction == Direction.NEUTRAL:
                     current_instruction.send_instruction()
                     print(current_instruction.direction)
-            previous_instruction = current_instruction
+            previous_instruction = current_instruction'''
 
         
